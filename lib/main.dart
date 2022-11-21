dd6efd3de489ac5f5eda50ced0c6529f98be6ec6
Christopher Bond <s3767838@students.rmit.edu.au>
2022-11-21 20:02:37 +1100

    prior to starting Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          title: Text('Demo'),
+        ),
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: 100.0,
+              color: Colors.red,
+              child: Text('Container 1'),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.yellow,
+                  child: Text('Container 2'),
+                ),
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.green,
+                  child: Text('Container 2'),
+                ),
+              ],
+            ),
+            Container(
+              width: 100.0,
+              color: Colors.blue,
+              child: Text('Container 3'),
+            ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
