181add926a2f59065653233a110dc40e9279b1f5
Christopher Bond <s3767838@students.rmit.edu.au>
2022-11-21 21:36:34 +1100

    finish of the business card app

--- lib/main.dart
+++ lib/main.dart
@@ -15,71 +15,74 @@ class MyApp extends StatelessWidget {
         ),
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/chris.jpg'),
-            ),
-            Text(
-              'Chris Bond',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 40.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text('TECHNICAL COORDINATOR',
-                style: TextStyle(
-                  fontFamily: 'SourceSansPro-Regular',
-                  fontSize: 20.0,
-                  color: Colors.teal[100],
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                )),
-            Container(
-                color: Colors.white,
-                padding: EdgeInsets.all(10.0),
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/chris.jpg'),
+                ),
+                Text(
+                  'Chris Bond',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text('TECHNICAL COORDINATOR',
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro-Regular',
+                      fontSize: 20.0,
+                      color: Colors.teal[100],
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                    )),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(color: Colors.teal[100]),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
                       Icons.phone,
                       color: Colors.teal,
                     ),
-                    SizedBox(
-                      width: 10.0,
+                    title: Text(
+                      '+61 407 353 954',
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
                     ),
-                    Text('+61 407 353 954',
-                        style: TextStyle(
-                          color: Colors.teal[900],
-                          fontFamily: 'SourceSansPro',
-                          fontSize: 20.0,
-                        ))
-                  ],
-                )),
-            Container(
-                color: Colors.white,
-                padding: EdgeInsets.all(10.0),
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
                       Icons.email,
                       color: Colors.teal,
                     ),
-                    SizedBox(
-                      width: 10.0,
+                    title: Text(
+                      'christopher@christopherbond.com.au',
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
                     ),
-                    Text('christopher@christopherbond.com.au',
-                        style: TextStyle(
-                          color: Colors.teal[900],
-                          fontFamily: 'SourceSansPro',
-                          fontSize: 20.0,
-                        ))
-                  ],
-                ))
-          ]),
+                  ),
+                ),
+              ]),
         ),
       ),
     );
